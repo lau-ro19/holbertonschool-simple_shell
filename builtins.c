af4f05d7b25ca0c80f4e699fd0d23b98a5c3d7b3
22niko-spw <nicosojd@gmail.com>
2026-04-23 23:56:48 +0200

    Add: a fonction that check if a command is a builtin and executes it

--- builtins.c
+++ builtins.c
@@ -15,3 +15,42 @@ void builtin_env(char **env)
 		i++;
 	}
 }
+/**
+ * builtin_exit - exit the shell
+ * @args: the array of arguments
+ * @env: the array of environment variable
+ */
+void builtin_exit(char **args, char **env)
+{
+	int status;
+
+	(void)env;
+	status = 0;
+	if (args[1] != NULL)
+		status = atoi(args[1]);
+	free(args);
+	exit(status);
+
+}
+/**
+ * check_builtin - checks if a command is a builtin and executes it
+ * @args: the array of arguments
+ * @env: the array of environment variables
+ *
+ * Return: 1 if builtin, 0 otherwise
+ */
+int check_builtin(char **args, char **env)
+{
+	if (strcmp(args[0], "exit") == 0)
+	{
+		builtin_exit(args, env);
+		return (1);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		builtin_env(env);
+		return (1);
+	}
+	return (0);
+
+}
