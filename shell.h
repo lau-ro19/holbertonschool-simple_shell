1a8c7cda55d48a4837a905cc464a6762cb4d81b1
22niko-spw <nicosojd@gmail.com>
2026-04-23 20:00:38 +0200

    Add: Shell.h, with prototype of each files

---
+++ shell.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+
+
+/* main.c*/
+int main(int argc, char **argv, char **env);
+
+/*parser.c*/
+char *read_line(void);
+char **tokenize(char *line);
+
+/*executor.c*/
+void executor(char *path, char **args, char **env);
+
+/*path.c*/
+char *find_path(char *command, char **env);
+
+/*builtin.c*/
+void builtin_exit(char **args, char **env);
+void builtin_env(char **env);
+int check_builtin(char **args, char **env);
+
+
+#endif
