4336c4628d695113242bbee23d25f686c25cabd2
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 15:28:51 +0200

    FIX: betty style for executor.c

--- executor.c
+++ executor.c
@@ -1,35 +1,37 @@
-#include"shell.h"
+#include "shell.h"
 
 /**
  * executor - Executes a command with given arguments and environment.
  * @path: Path to the command.
  * @args: Arguments for the command.
  * @env: Environment variables.
+ *
+ * Return: Exit status of the child process, or 1 on fork failure.
  */
-
 int executor(char *path, char **args, char **env)
 {
-    pid_t pid;
-    int status;
+	pid_t pid;
+	int status;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("fork");
-        return (1);
-    }
-    if (pid == 0)
-    {
-        execve(path, args, env);
-        perror("execve");
-        exit(127);
-        
-    }
-    else
-    {
-        waitpid(pid, &status, 0);
-    }
-    if (WIFEXITED(status))
-        return (WEXITSTATUS(status));
-    return (0);
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		execve(path, args, env);
+		perror("execve");
+		exit(127);
+	}
+	else
+	{
+		waitpid(pid, &status, 0);
+	}
+
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+
+	return (0);
 }
