d574a996d7d0541c7e6b066c4f8b2267164fc25e
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 14:37:42 +0200

    ADD: core tokenization logic Implemented the tokenize function within parser.c to transform raw input strings into an array of arguments (tokens) for the shell's ability to interpret and execute user commands.

--- parser.c
+++ parser.c
@@ -22,3 +22,35 @@ char *read_line(void)
  * @line: The input line to tokenize.
  * Return: An array of strings (tokens), or NULL on failure.
  */
+char **tokenize(char *line)
+{
+    int bufsize = 64,
+    int i = 0;
+    char **tokens = malloc(bufsize * sizeof(char *));
+    char *token;
+
+    if (!tokens)
+    {
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    token = strtok(line, " \t\r\n\a");
+    while (token != NULL)
+    {
+        tokens[i] = token;
+        i++;
+        if (i >= bufsize)
+        {
+            bufsize += 64;
+            tokens = realloc(tokens, bufsize * sizeof(char *));
+            if (!tokens)            
+            {
+                perror("realloc");
+                exit(EXIT_FAILURE); 
+            }
+        }
+        token = strtok(NULL, " \t\r\n\a");
+    }
+    tokens[i] = NULL;
+    return (tokens);
+}
