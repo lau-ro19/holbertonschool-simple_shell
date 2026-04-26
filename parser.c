00b5554716ba0a6717b3f253b9425fc346f87486
lau-ro19 <lroseantoinette@gmail.com>
2026-04-26 15:55:00 +0200

    FIX: betty style for the parser.c and fix for compilation failure

--- parser.c
+++ parser.c
@@ -1,64 +1,67 @@
-# include"shell.h"
+#include "shell.h"
 
 /**
  * read_line - Reads a line of input from stdin.
+ *
  * Return: A pointer to the read line, or NULL on failure.
  */
-
 char *read_line(void)
 {
-    char *line;
-    size_t n;
-    ssize_t chars;
+	char *line;
+	size_t n;
+	ssize_t chars;
 
-    line = NULL;
-    n = 0;
+	line = NULL;
+	n = 0;
 
-    chars = getline(&line, &n, stdin);
-    if (chars == -1)
-    {
-        free(line);
-        return (NULL);
-    }
-    return (line);
+	chars = getline(&line, &n, stdin);
+	if (chars == -1)
+	{
+		free(line);
+		return (NULL);
+	}
+	return (line);
 }
+
 /**
  * tokenize - Splits a line into tokens based on whitespace.
  * @line: The input line to tokenize.
+ *
  * Return: An array of strings (tokens), or NULL on failure.
  */
 char **tokenize(char *line)
-{ 
-    char **tokens;
-    char *token;
-    int bufsize;
-    int i ;
+{
+	char **tokens;
+	char *token;
+	int bufsize;
+	int i;
 
-    bufsize = 64;
-    i = 0;
-    tokens = malloc(bufsize * sizeof(char *));
+	bufsize = 64;
+	i = 0;
+	tokens = malloc(bufsize * sizeof(char *));
 
-    if (tokens == NULL)
-    {
-        return (NULL);
-    }
-    tokens = strtok(line, " \n\t");
-    
-    while (token != NULL)
-    {
-        tokens[i] = token;
-        i++;
-        if (i >= bufsize)
-        {
-            bufsize += 64;
-            tokens = realloc(tokens, bufsize * sizeof(char *));
-            if (tokens == NULL)            
-            {
-                return (NULL);
-            }
-        }
-        token = strtok(NULL, " \n\t");
-    }
-    tokens[i] = NULL;
-    return (tokens);
+	if (tokens == NULL)
+	{
+		return (NULL);
+	}
+
+	token = strtok(line, " \n\t");
+
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		if (i >= bufsize)
+		{
+			bufsize += 64;
+			tokens = realloc(tokens, bufsize * sizeof(char *));
+			if (tokens == NULL)
+			{
+				return (NULL);
+			}
+		}
+		token = strtok(NULL, " \n\t");
+	}
+	tokens[i] = NULL;
+	return (tokens);
 }
