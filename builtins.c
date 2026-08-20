7bba394d63a8f40b54b92c05aee2f39a1bf684d4
lau-ro19 <lroseantoinette@gmail.com>
2026-08-20 19:46:55 +0200

    docs(builtins): add pseudocode for exit command

--- builtins.c
+++ builtins.c
@@ -7,3 +7,6 @@ int check_builtin(char **args, char **env)
 
 	return (0);
 }
+omparer args[0] avec la chaîne "exit" si args[0] est égal à "exit":
+    - Libérer la mémoire allouée si nécessaire
+    - Quitter le programme immédiatement avec exit(0)
