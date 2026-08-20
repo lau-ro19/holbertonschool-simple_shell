af3e7be1cb7e4e552b5c331322c31610fb01d7e5
lau-ro19 <lroseantoinette@gmail.com>
2026-08-20 17:27:20 +0200

    docs(executor): add pseudocode for fork process creation

--- executors.c
+++ executors.c
@@ -7,3 +7,8 @@ int execute_command(char **args, char **env)
 
 	return (0);
 }
+Déclarer la variable pid
+Appeler fork()
+Si pid < 0 (échec de fork):
+    Afficher une erreur avec perror
+    Retourner (-1)
