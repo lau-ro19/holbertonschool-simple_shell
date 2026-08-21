d79f4b1097356dd9bcb09fbc80b74a11fa8d0520
lau-ro19 <lroseantoinette@gmail.com>
2026-08-21 10:34:25 +0200

    fix(header): update execute_command prototype and add check_builtin

--- shell.h
+++ shell.h
@@ -13,7 +13,8 @@ char *build_path(char *dir, char *command);
 char *search_in_path(char *command);
 char *read_command(void);
 char *get_valid_path(char *command);
-int execute_command(char *path, char **argv);
+int execute_command(char *path, char **args, char **env);
+int check_builtin(char **args, char **env);
 char **split_line(char *line);
 void free_argv(char **argv);
 
